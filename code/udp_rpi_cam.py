05bbdb21bc49ee29f7b33c2e1aa03b430d3afbd1
Wes Tarro <dudamoos@users.noreply.github.com>
2016-04-28 11:14:09 -0400

    Demo3: Changed video module to use PiCamera for weight concerns

--- code/udp_rpi_cam.py
+++ code/udp_rpi_cam.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python -O
 from __future__ import print_function
 
-import shared
-import udp
-import time
+import shared, udp, time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import cv2
 
 transport = udp.UdpSocket(shared.PC_CAM_ADDR)
-capture = cv2.VideoCapture(0)
-
-PERIOD = 1.0/10
-
-try:
-	while True:
-		next_time = time.time() + PERIOD
-		success, image = capture.read()
-		if not success:
-			print("Warning: Couldn't read video frame!")
-			continue
-		image = cv2.resize(image, (320, 240))
-		transport.send_big(shared.img2bufz(image))
-		throttle_delay = next_time - time.time()
-		if throttle_delay > 0: time.sleep(throttle_delay)
-except KeyboardInterrupt:
-	capture.release()
+with PiCamera(resolution=(320, 240), framerate=10) as camera:
+	raw_cap = PiRGBArray(camera, size=(320, 240))
+	time.sleep(2)
+	for frame in camera.capture_continuous(raw_cap, format="bgr", use_video_port=True):
+		transport.send_big(shared.img2bufz(raw_cap.array))
+		raw_cap.truncate(0)
+		print("Sent a frame!")
